980bbbff2260271a96503a05a1e467c07cb520f9
Filipe Costa <filipebarcos@gmail.com>
2012-01-26 00:11:27 -0300

    Project skeleton generated by aptana studio 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CpmService::Application.config.secret_token = 'f023f053e3864b499686979a2566f4ec9dc17a64626764607d53175a149267204eec3513a066d84ff4af60094be3c009e88cb4873486178c78b43bea14720414'
